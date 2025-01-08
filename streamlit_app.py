0b82504ef766c0a8eb4a2cd94a9a706bed26906b
tlangston1234 <tlangston@deloitte.com>
2025-01-08 10:30:08 -0600

    Update streamlit_app.py
    Fixed container width typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
-        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=TRUE)
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
 
